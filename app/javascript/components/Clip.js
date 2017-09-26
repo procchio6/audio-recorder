ceb202ec6da99374cc9ef401030d08464018cff1
Pat Rocchio <procchio6@gmail.com>
2017-09-25 20:05:12 -0400

    Style delete button

--- app/javascript/components/Clip.js
+++ app/javascript/components/Clip.js
@@ -6,8 +6,8 @@ class Clip extends Component {
       <div>
         <h2>{this.props.title}</h2>
         <audio src={this.props.audio} controls></audio>
-        <button onClick={(e) => this.props.onDelete(this.props.id)}>
-          Delete
+        <button className='btn-delete' onClick={(e) => this.props.onDelete(this.props.id)}>
+          X
         </button>
       </div>
     );
